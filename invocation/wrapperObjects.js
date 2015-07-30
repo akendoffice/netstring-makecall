59b29eb264212b83627e6c4f5c68529acc679955
aelengovan <aelengovan@paypal.com>
2015-07-30 13:05:37 +0530

    TCP request object

--- invocation/wrapperObjects.js
+++ invocation/wrapperObjects.js
@@ -41,6 +41,34 @@ function TCPRequestObject(){
 	this.app_name = "";
 	this.correlation_id = "";
 	this.secret = "";
-	this.params;
+	this.cipherDataObject;
 
-}
+	this.setOperation = function (operation) {
+		this.operation = operation;
+	};
+
+	this.setVersion = function (version){
+		this.version = version;
+	};
+
+	this.setRequestId = function (requestId) {
+		this.request_id = requestId;
+	};
+
+	this.setAppName = function (app_name) {
+		this.app_name = app_name;
+	};
+
+	this.setCorrelationId = function (correlation_id) {
+		this.correlation_id = correlation_id;
+	};
+
+	this.setSecret = function(secret){
+		this.secret = secret;
+	};
+
+	this.setCipherDataObject = function (params) {
+		cipherDataObject = params;
+	};
+
+};
