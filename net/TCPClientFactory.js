94c3478c6f954e2f423329a4245fe4452ac2d1c4
aelengovan <aelengovan@paypal.com>
2015-07-31 13:24:47 +0530

    Done with simply TCP makecall yet to add pooling capability

--- net/TCPClientFactory.js
+++ net/TCPClientFactory.js
@@ -1,2 +1,35 @@
 'use strict';
 
+module.exports = {
+	makeTCPCall: makeTCPCall
+};
+
+function makeTCPCall(request, response){
+
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 23456;
+var res;
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function() {
+
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    client.write(request);
+
+});
+
+//'data' event handler for the client socket
+client.on('data', function(data) {
+    res = data.toString('utf-8');
+    // Close the client socket completely
+    client.destroy(); 
+});
+
+//'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+    return response(res);
+});
+
+}
--- test/SomeClient_Integration.js
+++ test/SomeClient_Integration.js
@@ -1,29 +1,27 @@
 // KMSClient.js
 
 var wrapObjects = require('../invocation/TransportWrapper');
-//var Netstring = require('netstring-stream');
 var netstr = require('../net/NetstringObject');
+var tcpCall = require('../net/TCPClientFactory');
 
 describe('printOutFinalObjectInString', function(){
   it('type1', function(){
   	var cipherDataObject = new wrapObjects.CipherDataObject();
-    cipherDataObject.setKeyId("encrypted_test_cryptutil_crypt_key");
+    cipherDataObject.setKeyId("some_key_name");
     cipherDataObject.setEncodedData(["YWJjZHBxcnM=","YWJjZGdoYnk="]);
   	cipherDataObject.setIsPKCS7(false);
 
   	var tcpObj = new wrapObjects.TCPRequestObject();
     tcpObj.setOperation("encrypt");
     tcpObj.setVersion(1);
-  	tcpObj.setAppName("keymakerapitest");
+  	tcpObj.setAppName("my_application_name");
     tcpObj.setCorrelationId("abcd1234");
-  	tcpObj.setSecret("JGzi?QX_TVQI7qJ6ck0bW_#pd4Dk_FLM");
+  	tcpObj.setSecret("some_secret");
 
 
     tcpObj.setParams(cipherDataObject);
   	console.log("Pure TCPRequestObject :");
     console.log(JSON.stringify(tcpObj));
-    //Using netstring-stream
-    //var bytes = System.Text.Encoding.UTF8.GetBytes(JSON.stringify(tcpObj));
 
     /** works ***/
     // console.log("Before NetString conversion");
@@ -36,8 +34,14 @@ describe('printOutFinalObjectInString', function(){
     var netStringData = netStrObj.convertToNetStringBuffer();
     console.log("Netstring in Str format: ");
     console.log(netStringData.toString('utf-8'));
-    console.log("NetString in hex format: ");
-    console.log(netStringData.toString('hex'));
+    //console.log("NetString in hex format: ");
+    //console.log(netStringData.toString('hex'));
+
+    //Make the TCP call using NetString Protocol
+    tcpCall.makeTCPCall(netStringData, response);
+    function response(res){
+      console.log("In Client : \n" + res);
+    }
 
   });
 })
